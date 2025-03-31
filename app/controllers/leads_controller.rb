11eddc2ff2598ce46e092337bc08fd8af3694a58
Daniel Levelev <levelev@gmail.com>
2025-03-31 21:33:06 +0200

    name, status, kunde, view in close

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -27,7 +27,7 @@ class LeadsController < ApplicationController
           }
         ]
       },
-      _fields: { lead: ['id', 'name', 'status_label'] },
+      _fields: { lead: ['id', 'name', 'status_label', 'description', 'contacts', 'custom.cf_K3ausfM3pWXa7CAMQIpWV0eV7Dqc2IXIuOpiX4Luqa9'] },
       _limit: 200
     }
 
@@ -40,7 +40,7 @@ class LeadsController < ApplicationController
       []
     end
   end
-  
+
 
 end
 
