b2e8504272d11a8a7b0441b0e38e53623705d826
Daniel Levelev <levelev@gmail.com>
2025-04-03 22:21:18 +0200

    working on status

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -4,58 +4,75 @@ class LeadsController < ApplicationController
 
   def index
     @leads = fetch_leads_by_smartview
+    @lead_statuses = fetch_lead_statuses
   end
 
   def update_description
-  lead_id = params[:id]
-  new_description = params[:description]
+    lead_id = params[:id]
+    new_description = params[:description]
 
-  conn = Faraday.new(url: 'https://api.close.com/api/v1/') do |faraday|
-    faraday.request :authorization, :basic, CLOSE_API_KEY, ''
-    faraday.adapter Faraday.default_adapter
+    conn = close_connection
+
+    response = conn.put(
+      "lead/#{lead_id}/",
+      { description: new_description }.to_json,
+      'Content-Type' => 'application/json'
+    )
+
+    if response.success?
+      render json: { status: 'ok' }
+    else
+      Rails.logger.error "Close API Error: #{response.status} - #{response.body}"
+      render json: { status: 'error', message: response.body }, status: :unprocessable_entity
+    end
   end
 
-  response = conn.put(
-    "lead/#{lead_id}/",
-    { description: new_description }.to_json,
-    'Content-Type' => 'application/json'
-  )
+  def update_status
+    lead_id = params[:id]
+    new_status_id = params[:status_id]
 
-  if response.success?
-    render json: { status: 'ok' }
-  else
-    Rails.logger.error "Close API Error: #{response.status} - #{response.body}"
-    render json: { status: 'error', message: response.body }, status: :unprocessable_entity
+    conn = close_connection
+
+    response = conn.put(
+      "lead/#{lead_id}/",
+      { status_id: new_status_id }.to_json,
+      'Content-Type' => 'application/json'
+    )
+
+    if response.success?
+      render turbo_stream: turbo_stream.replace(
+        "lead_status_#{lead_id}",
+        partial: "leads/status",
+        locals: {
+          lead: { 'id' => lead_id, 'status_id' => new_status_id },
+          lead_statuses: fetch_lead_statuses
+        }
+      )
+    else
+      Rails.logger.error "Close API Error: #{response.status} - #{response.body}"
+      render json: { status: 'error', message: response.body }, status: :unprocessable_entity
+    end
   end
-end
-
-
-
-
-
-
-
 
   private
 
   def fetch_leads_by_smartview
-    conn = Faraday.new(url: 'https://api.close.com/api/v1/') do |faraday|
-      faraday.request :authorization, :basic, CLOSE_API_KEY, ''
-      faraday.adapter Faraday.default_adapter
-    end
+    conn = close_connection
 
     search_query = {
       query: {
         type: 'and',
         queries: [
           { type: 'object_type', object_type: 'lead' },
-          {
-            type: 'any_of_saved_search',
-            saved_search_ids: [SMART_VIEW_ID]
-          }
+          { type: 'any_of_saved_search', saved_search_ids: [SMART_VIEW_ID] }
         ]
       },
-      _fields: { lead: ['id', 'name', 'status_label', 'description', 'contacts', 'custom.cf_K3ausfM3pWXa7CAMQIpWV0eV7Dqc2IXIuOpiX4Luqa9', 'date_created', 'custom.cf_95xSZmN3OrCcUBVrU3M0QDn9T2peoKz4q9n1HHV5I1A'] },
+      _fields: {
+        lead: [
+          'id', 'name', 'status_label', 'status_id', 'description',
+          'contacts', 'custom.cf_K3ausfM3pWXa7CAMQIpWV0eV7Dqc2IXIuOpiX4Luqa9', 'date_created'
+        ]
+      },
       _limit: 200
     }
 
@@ -63,13 +80,28 @@ end
     if response.success?
       JSON.parse(response.body)['data']
     else
-      puts "Close API Error: #{response.status}"
-      puts "Antwort: #{response.body}"
+      Rails.logger.error "Close API Error: #{response.status} - #{response.body}"
       []
     end
   end
 
+  def fetch_lead_statuses
+    conn = close_connection
 
+    response = conn.get('status/lead/')
+
+    if response.success?
+      JSON.parse(response.body)['data']
+    else
+      Rails.logger.error "Close API Error (statuses): #{response.status} - #{response.body}"
+      []
+    end
+  end
+
+  def close_connection
+    Faraday.new(url: 'https://api.close.com/api/v1/') do |faraday|
+      faraday.request :authorization, :basic, CLOSE_API_KEY, ''
+      faraday.adapter Faraday.default_adapter
+    end
+  end
 end
-
-
