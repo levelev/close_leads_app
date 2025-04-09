89673fd43f62aba922cd7e11fc2b1737ec4232d8
Eli Block <18074026+eliblocks@users.noreply.github.com>
2025-04-08 21:20:53 -0400

    initialize theme

--- vendor/assets/javascripts/initTheme.js
+++ vendor/assets/javascripts/initTheme.js
@@ -2,6 +2,5 @@ const body = document.body;
 const theme = localStorage.getItem('theme')
 
 if (theme) {
-  console.log("initializing theme")
   document.documentElement.setAttribute('data-bs-theme', theme)
 }
